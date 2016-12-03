7733a065c9b5cae15b806e54aa86d48633d77648
Ravin Sardal <ravin.sardal@gmail.com>
2016-12-03 12:59:04 -0800

    Write delay_inner loop

--- main.s
+++ main.s
@@ -8,8 +8,11 @@
 .text
 _main:
 	MOV #__SP_init, W15
-	MOV #0x0000, W1
-	MOV #0xFFFF, W2
+	MOV #0x0000, W1	    ;used for delay_inner loop counter
+	MOV #0xFFFF, W2	    ;used for delay_inner loop max terminator
+	MOV #0x0000, W3	    ;used for delay loop counter
+	MOV #0x000A, W4	    ;used for delay loop max terminator
+	
 init_ports:
 	BCLR TRISA, #2
 	BCLR TRISA, #4
@@ -19,38 +22,22 @@ init_ports:
 routine_helicopter:	
 	BSET LATA, #2
 	CALL delay
-	CALL delay
-	CALL delay
 	BCLR LATA, #2
-	CALL delay
-	CALL delay
 	CALL delay
 	
 	BSET LATA, #4
 	CALL delay
-	CALL delay
-	CALL delay
 	BCLR LATA, #4
-	CALL delay
-	CALL delay
 	CALL delay
 	
 	BSET LATB, #1
 	CALL delay
-	CALL delay
-	CALL delay
 	BCLR LATB, #1
-	CALL delay
-	CALL delay
 	CALL delay
 	
 	BSET LATA, #1
 	CALL delay
-	CALL delay
-	CALL delay
 	BCLR LATA, #1
-	CALL delay
-	CALL delay
 	CALL delay
 	
 	BRA routine_helicopter
@@ -58,10 +45,18 @@ routine_helicopter:
 	
 	BRA done
 
-delay: 
+delay:
+    CALL delay_inner
+    ADD #0x0001, W3
+    CP W3, W4
+    BRA NZ, delay
+    MOV #0x0000, W3
+    return
+	
+delay_inner: 
 	ADD #0x0001, W1
 	CP W1, W2
-	BRA NZ, delay
+	BRA NZ, delay_inner
 	MOV #0x0000, W1
 	return
 	
