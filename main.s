3abf86d5f7d2068fa46bcc0246e7b544fba99366
Ravin Sardal <ravin.sardal@gmail.com>
2016-12-03 12:48:03 -0800

    Implement helicopter routine

--- main.s
+++ main.s
@@ -10,26 +10,52 @@ _main:
 	MOV #__SP_init, W15
 	MOV #0x0000, W1
 	MOV #0xFFFF, W2
-start:
+init_ports:
+	BCLR TRISA, #2
+	BCLR TRISA, #4
+	BCLR TRISB, #1
 	BCLR TRISA, #1
+	
+routine_helicopter:	
+	BSET LATA, #2
+	CALL delay
+	CALL delay
+	CALL delay
+	BCLR LATA, #2
+	CALL delay
+	CALL delay
+	CALL delay
+	
+	BSET LATA, #4
+	CALL delay
+	CALL delay
+	CALL delay
+	BCLR LATA, #4
+	CALL delay
+	CALL delay
+	CALL delay
+	
+	BSET LATB, #1
+	CALL delay
+	CALL delay
+	CALL delay
+	BCLR LATB, #1
+	CALL delay
+	CALL delay
+	CALL delay
+	
 	BSET LATA, #1
 	CALL delay
 	CALL delay
 	CALL delay
-	CALL delay
-	CALL delay
-	CALL delay
-	CALL delay
-	CALL delay
-	CALL delay
-	CALL delay
-	CALL delay
-	CALL delay
-	CALL delay
-	CALL delay
-	CALL delay
-	CALL delay
 	BCLR LATA, #1
+	CALL delay
+	CALL delay
+	CALL delay
+	
+	BRA routine_helicopter
+	
+	
 	BRA done
 
 delay: 
