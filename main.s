33dd25c192e3e168f737b4ed2e5a99af04deaa24
Ravin Sardal <ravin.sardal@gmail.com>
2016-12-03 12:15:52 -0800

    Remove uneeded labels, call deplay mutliple times to ensure long delay

--- main.s
+++ main.s
@@ -10,31 +10,35 @@ _main:
 	MOV #__SP_init, W15
 	MOV #0x0000, W1
 	MOV #0xFFFF, W2
-	MOV #0x0000, W3 ;w3 used to represent the state of the LED
 start:
-	BCLR TRISA, #0
-	
-led_on:
-	MOV #0XFFFF, W0
-	;MOV W0, LATA
-	BSET LATA, #0
-	BRA delay
-	
-led_off:
-	MOV #0X0000, W0
-	;MOV W0, LATA
-	BCLR LATA, #0
-	bra delay
-	
+	BCLR TRISA, #1
+	BSET LATA, #1
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	CALL delay
+	BCLR LATA, #1
+	BRA done
+
 delay: 
 	ADD #0x0001, W1
 	CP W1, W2
 	BRA NZ, delay
 	MOV #0x0000, W1
-	CP W0, W3
-	BRA Z, led_on
-	BRA led_off
-
+	return
+	
 done:	goto done
 .end
 
